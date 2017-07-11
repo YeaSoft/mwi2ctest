35b4b0dbdffc0bc1abf937b183da42a3245a1888
Dominik Schloesser <dsc@dosc.net>
2017-07-11 19:44:04 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+MeisterWerk i2ctesting ground
+=============================
+
+The following project is a playground for testing the MeisterWerk i2c-Framework.
+
---
+++ src/app.cpp
@@ -0,0 +1,66 @@
+// app.cpp - Sample Application 1
+//
+// This small sample application built with
+// the MeisterWerk Framework flashes the
+// internal LED
+
+// platform includes
+#include <ESP8266WiFi.h>
+
+// Let MeisterWerk output debugs on Serial
+#define DEBUG 1
+
+// framework includes
+#include <MeisterWerk.h>
+#include <base/i2cbus.h>
+
+using namespace meisterwerk;
+
+/*
+// led class
+class MyLed : public core::entity {
+    public:
+    unsigned long ledLastChange       = 0;
+    unsigned long ledBlinkIntervallMs = 500;
+    uint8_t       pin                 = BUILTIN_LED;
+    bool          state               = false;
+
+    MyLed( String _name, uint8_t _pin ) : core::entity( _name ) {
+        pin = _pin;
+    }
+
+    virtual void onSetup() {
+        pinMode( pin, OUTPUT );
+    }
+
+    virtual void onLoop( unsigned long ticker ) {
+        unsigned long millis = ( ticker - ledLastChange ) / 1000L;
+        if ( millis >= ledBlinkIntervallMs ) {
+            ledLastChange = ticker;
+            if ( state ) {
+                state = false;
+                digitalWrite( pin, HIGH );
+            } else {
+                state = true;
+                digitalWrite( pin, LOW );
+            }
+        }
+    }
+};
+*/
+// application class
+class MyApp : public core::baseapp {
+    public:
+    MyApp()
+        : core::baseapp( "MyApp" ) {
+    }
+
+    virtual void onSetup() {
+        // Debug console
+        Serial.begin( 115200 );
+
+    }
+};
+
+// Application Instantiation
+MyApp app;
