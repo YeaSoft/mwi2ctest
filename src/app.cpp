aa36f6f012f78eca3514e1ecb977127f39f01e98
Dominik Schloesser <dsc@dosc.net>
2017-08-02 08:49:39 +0200

    app luminosity, humidity and latest lib

--- src/app.cpp
+++ src/app.cpp
@@ -34,6 +34,7 @@
 #include <../thing/i2cdev-TSL2561.h>
 #include <../thing/mqtt.h>
 #include <../thing/ntp.h>
+#include <../thing/temp-hum_DHT11_22.h>
 
 using namespace meisterwerk;
 
@@ -48,21 +49,26 @@ class MyLed : public core::entity {
     unsigned long   frameDelta;
     long            directionalDelta;
     unsigned long   pwmrange;
+    unsigned long   initPwmrange;
     int             ledLevel = 0;
     util::metronome frame;
     uint8_t         pin   = BUILTIN_LED;
     bool            state = false;
+    float           ledMaxBright;
 
     MyLed( String name, uint8_t pin, ledmode ledMode = ledmode::STATIC,
            unsigned long ledBlinkIntervalMs = 500L )
         : core::entity( name, 50000 ), pin{pin}, frame( frameRate ),
           blinker( ledBlinkIntervalMs ), ledMode{ledMode}, ledBlinkIntervalMs{ledBlinkIntervalMs} {
         pinMode( pin, OUTPUT );
-        pwmrange = 96; // PWMRANGE;
-        ledLevel = 0;
+        initPwmrange = 96; // PWMRANGE;
+        pwmrange     = initPwmrang;
+        ledMaxBright = 1023.0;
+        ledLevel     = 0;
     }
 
-    void configureFrames() {
+    getPwmRangeFromLumi( float luminosity ) {
+        pwmrange         = initPwmrange * luminosity / 1023.0;
         frameDelta       = pwmrange * frameRate / ledBlinkIntervalMs;
         directionalDelta = frameDelta;
 
@@ -73,6 +79,9 @@ class MyLed : public core::entity {
             DBG( "Framerate excessively high, register entity with slower slices." );
             frameDelta = 1;
         }
+    }
+    void configureFrames() {
+        getPwmRangeFromLumi( ledMaxBright );
         digitalWrite( pin, HIGH ); // OFF
         DBG( "Led on pin " + String( pin ) );
         state    = false;
@@ -83,6 +92,11 @@ class MyLed : public core::entity {
         frameRate = 50000 / 1000L;
         frame.setlength( frameRate );
         configureFrames();
+        subscribe( "*/luminosity" );
+        subscribe( entName + "/mode/set" );
+        //subscribe( entName + "/mode/get" );
+        subscribe( entName + "/state/set" );
+        //subscribe( entName + "/state/get" );
         log( T_LOGLEVEL::INFO, "Up and running" );
     }
 
@@ -101,6 +115,20 @@ class MyLed : public core::entity {
         state   = ledState;
         if ( state ) {
             digitalWrite( pin, LOW ); // ON
+        } else {
+            digitalWrite( pin, HIGH ); // OFF
+        }
+    }
+
+    void setLed( float ledBriState ) { // 0..1.0;
+        ledMode = ledmode::STATIC;
+        if ( ledBriState > 0.0 )
+            state = true;
+        else
+            state = false;
+
+        if ( state ) {
+            analogWrite( pin, ledBrightState * 1024.0 ); // ON
         } else {
             digitalWrite( pin, HIGH ); // OFF
         }
@@ -151,9 +179,83 @@ class MyLed : public core::entity {
             }
         }
     }
+    virtual void receive( const char *origin, const char *ctopic, const char *msg ) override {
+        String topic( ctopic );
+        int    p  = topic.indexOf( "/" );
+        String t1 = topic.substring( p + 1 );
+        if ( t1 == "luminosity" ) {
+            DBG( "Luminosity msg: " + String( msg ) );
+            DynamicJsonBuffer jsonBuffer( 200 );
+            JsonObject &      root = jsonBuffer.parseObject( msg );
+            if ( !root.success() ) {
+                DBG( "Led/luminosity: Invalid JSON received: " + String( msg ) );
+                return;
+            }
+            float lx     = atof( root["luminosity"] );
+            ledMaxBright = ( lx * 1023.0 / 1000.0 );
+            if ( ledMaxBright > 1023 )
+                ledMaxBright = 1023;
+            if ( lcdBright < 1 )
+                ledMaxBright = 1;
+        }
+        if ( topic == entName + "/state/set" ) {
+            DBG( "state msg: " + String( msg ) );
+            DynamicJsonBuffer jsonBuffer( 200 );
+            JsonObject &      root = jsonBuffer.parseObject( msg );
+            if ( !root.success() ) {
+                DBG( "Led/state: Invalid JSON received: " + String( msg ) );
+                return;
+            }
+            String stateStr = atof( root["state"] );
+            if ( state == "off" ) {
+                setState( false );
+            } else if ( state == "on" ) {
+                float bri = atof( root["brightness"] );
+                if ( bri > 0.0 ) {
+                    setState( bri );
+
+                } else {
+                    setState( true );
+                }
+            }
+
+            ledMaxBright = ( lx * 1023.0 / 1000.0 );
+            if ( ledMaxBright > 1023 )
+                ledMaxBright = 1023;
+            if ( lcdBright < 1 )
+                ledMaxBright = 1;
+        }
+        if ( topic == entName + "/mode/set" ) {
+            DBG( "Mode msg: " + String( msg ) );
+            DynamicJsonBuffer jsonBuffer( 200 );
+            JsonObject &      root = jsonBuffer.parseObject( msg );
+            if ( !root.success() ) {
+                DBG( "Led/mode: Invalid JSON received: " + String( msg ) );
+                return;
+            }
+            String stateStr = atof( root["state"] );
+            if ( state == "off" ) {
+                setState( false );
+            } else if ( state == "on" ) {
+                float bri = atof( root["brightness"] );
+                if ( bri > 0.0 ) {
+                    setState( bri );
+
+                } else {
+                    setState( true );
+                }
+            }
+
+            ledMaxBright = ( lx * 1023.0 / 1000.0 );
+            if ( ledMaxBright > 1023 )
+                ledMaxBright = 1023;
+            if ( lcdBright < 1 )
+                ledMaxBright = 1;
+        }
+    }
 };
 
-//[Lcd+]
+//[Lcd+]                                  dead LED
 // Vin GND Rst En  3V3 GND SCK SD0 CMD SD1 D11 D12 RSV RSV A0   (ESP pins)
 //                                         GP9 G10              (GPIOs)
 // [ ]Rst                         +----------------------+ =|
@@ -163,7 +265,7 @@ class MyLed : public core::entity {
 // [ ]Flsh                        +----------------------+ =|
 //         GP1 GP3 G15 G13 G12 G14         GP2 GP0 GP4 Gp5 G16  (GPIOs)
 // 3V3 GND Tx  Rx  D8  D7  D6  D5  GND 3V3 D4  D3  D2  D1  D0   (ESP pins)
-//                    [TX][RX]            [Sw]   [sda|scl][led]
+//                    [TX][RX] DHT         [Sw]   [sda|scl][led]
 //                    SwSerial           Switch   I2C-Bus  intl
 //   convention: SCL:yellow cable, SDA:green cable.
 
@@ -186,9 +288,11 @@ class MyApp : public core::baseapp {
     String temperature;
     String pressure;
     String luminosity;
+    String humidity;
     String isoTimeTemp;
     String isoTimePress;
     String isoTimeLumi;
+    String isoTimeHumidity;
     int    noSat = 0;
     /*
     thing::i2cdev_LCD_2_4_16_20 i2cd2;
@@ -204,9 +308,10 @@ class MyApp : public core::baseapp {
     thing::GPS_NEO_6M          gps;
     thing::Ntp                 ntpcl;
     thing::mqtt                mqttcl;
-    thing::dcf77               dcf;
-    thing::i2cdev_RTC_DS3231   hprtc;
-    thing::i2cdev_TSL2561      lumi;
+    // thing::dcf77               dcf;
+    thing::i2cdev_RTC_DS3231 hprtc;
+    thing::i2cdev_TSL2561    lumi;
+    thing::dht               dhtxx;
 
     MyApp()
         : core::baseapp( "SensorClock-II", 50000 ),
@@ -216,9 +321,10 @@ class MyApp : public core::baseapp {
           /* i2cd2( "D2", 0x25, "2x16" ), */ i2cd3(
               "D3", 0x26, 4, 20 ), /* i2cd4( "D4", 0x27, "2x16" ), i2cd5( "D5", 0x71 ) */
           bmp180( "bmp180", 0x77 ), i2coled( "D2", 0x3c, 64, 128 ), mtm( "mastertime" ),
-          gps( "gps", D6, D7 ), wnet( "net" ), ntpcl( "ntpcl" ), mqttcl( "mqttcl" ), dcf( "dcf77" ),
+          gps( "gps", D6, D7 ), wnet( "net" ), ntpcl( "ntpcl" ),
+          mqttcl( "mqttcl" ), /* dcf( "dcf77" ), */
           // hprtc( "hp-rtc", "DS3231", 0x68 ) {
-          hprtc( "hp-rtc", "DS1307", 0x68 ), lumi( "lumi", 0x39 ) {
+          hprtc( "hp-rtc", "DS1307", 0x68 ), lumi( "lumi", 0x39 ), dhtxx( "dht", "DHT22", D5 ) {
     }
 
     bool readAppConfig() {
@@ -265,6 +371,7 @@ class MyApp : public core::baseapp {
         subscribe( "*/temperature" );
         subscribe( "*/pressure" );
         subscribe( "*/luminosity" );
+        subscribe( "*/humidity" );
         subscribe( "mastertime/time/set" );
         subscribe( "Apple/pricerealtime" );
         subscribe( "*/gps" );
@@ -364,6 +471,11 @@ class MyApp : public core::baseapp {
             isoTimeLumi = root["time"].as<char *>();
             luminosity  = root["luminosity"].as<char *>();
             log( T_LOGLEVEL::INFO, "Luminosity: " + luminosity + "lux", "sensors" );
+        }
+        if ( t1 == "humidity" ) {
+            isoTimeHumidity = root["time"].as<char *>();
+            humidity        = root["humidity"].as<char *>();
+            log( T_LOGLEVEL::INFO, "Humidity: " + humidity + "%", "sensors" );
         }
         if ( t1 == "gps" ) {
             noSat = root["satellites"];
